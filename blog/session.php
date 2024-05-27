184a06adcd8fe1a9a0b3cc1365c8d6615a0446af
Şevval Yavuz <111804052+ssevvalyavuz@users.noreply.github.com>
2024-05-27 20:42:52 +0300

    Update session.php

--- blog/session.php
+++ blog/session.php
@@ -1,22 +1,16 @@
 <?php
-// Oturumu başlat
 session_start();
 
-// Oturum süresini ayarla (isteğe bağlı)
-// Örneğin, 1 saatlik bir oturum süresi
-$session_duration = 3600; // 3600 saniye = 1 saat
+// Oturumun açık kalma süresini belirliyoruz
+$session_duration = 3600;
 ini_set('session.gc_maxlifetime', $session_duration);
 session_set_cookie_params($session_duration);
 
-// Kullanıcı oturumu başarıyla başlatıldıysa
 if (isset($_SESSION['user_id'])) {
-    // Kullanıcı giriş yapmış demektir, istediğiniz ek işlemleri yapabilirsiniz
-    // Örneğin, kullanıcıya hoş geldiniz mesajı gösterebilirsiniz
     $user_id = $_SESSION['user_id'];
     $username = $_SESSION['username'];
     echo "Hoş geldiniz, $username!";
 } else {
-    // Kullanıcı oturumu yoksa, isterseniz giriş yapmaları için bir bağlantı gösterebilirsiniz
     echo "Lütfen giriş yapın.";
 }
 ?>
