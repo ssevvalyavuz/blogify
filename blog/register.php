daaf11d1cb4b90171a1b6ab6af27995053990077
Şevval Yavuz <111804052+ssevvalyavuz@users.noreply.github.com>
2024-05-27 20:41:48 +0300

    Update register.php

--- blog/register.php
+++ blog/register.php
@@ -1,23 +1,22 @@
 <?php
-// Hata raporlamasını aç
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// Veritabanı bağlantısını dahil et
 include 'db.php';
 include 'header.php';
 
-// Form gönderildiğinde
+// Kayıt ol tuşuna basılınca çalışacak yer
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // Form verilerini al
+    // Girilen bilgileri çekiyoruz
     $fullName = $_POST['fullName'];
     $username = $_POST['username'];
     $email = $_POST['email'];
     $phone = $_POST['phone'];
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-    // Veritabanına kaydetme işlemini gerçekleştir
+    // Veritabanına kullanıcıyı kayıt ediyoruz.
     $query = "INSERT INTO users (fullName, username, email, phone, password) VALUES (?, ?, ?, ?, ?)";
     $stmt = $conn->prepare($query);
     if ($stmt === false) {
@@ -27,9 +26,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     if ($stmt->execute()) {
         echo "Kayıt başarıyla oluşturuldu.";
-        // Kayıt işlemi başarılı olduğunda kullanıcıyı yönlendirme yapabilirsiniz.
-        header("Location: login.php"); // Örneğin, kayıt işlemi tamamlandığında kullanıcıyı giriş sayfasına yönlendirir.
-        exit; // Yönlendirme yaptıktan sonra betik çalışmasını sonlandırır.
+        header("Location: login.php"); 
+        exit;
     } else {
         echo "Hata: " . $stmt->error;
     }
