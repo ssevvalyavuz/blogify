a819a2c6bd713f83e50ceb7a8d122e7d5e26e64f
Şevval Yavuz <111804052+ssevvalyavuz@users.noreply.github.com>
2024-05-27 20:33:03 +0300

    Update config.php

--- blog/config.php
+++ blog/config.php
@@ -5,14 +5,11 @@ $username = "your_username"; // Veritabanı kullanıcı adı
 $password = "your_password"; // Veritabanı şifresi
 $database = "your_database"; // Kullanılacak veritabanının adı
 
-// Veritabanı bağlantısı
 $conn = new mysqli($servername,$username,$password,$database);
 
-// Bağlantı hatası kontrolü
 if ($conn->connect_error) {
     die("Veritabanına bağlanırken hata oluştu: " . $conn->connect_error);
 }
 
-// Türkçe karakter desteği
 $conn->set_charset("utf8");
 ?>
