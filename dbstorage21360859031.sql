34fefde094730e0826ad68d4305d1e0a3c2ef885
Şevval Yavuz <111804052+ssevvalyavuz@users.noreply.github.com>
2024-05-28 15:24:26 +0300

    database.sql

--- dbstorage21360859031.sql
+++ dbstorage21360859031.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `dbstorage21360859031`
+-- Database: `YOUR_DATABASE`
 --
 
 -- --------------------------------------------------------
