d2aa2d1ddd57d755737a15535bbf801f03e95089
Şevval Yavuz <111804052+ssevvalyavuz@users.noreply.github.com>
2024-05-28 15:26:35 +0300

    Update scripts.js

--- blog/js/scripts.js
+++ blog/js/scripts.js
@@ -1,7 +1,4 @@
 $(document).ready(function() {
-    // Sayfa yüklendiğinde yapılacak işlemler buraya yazılabilir
-
-    // Örnek: Bir düğmeye tıklandığında bir alert kutusu göster
     $('#myButton').click(function() {
         alert('Button clicked!');
     });
